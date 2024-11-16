16b457a269b7f3dbdd90e7c211195eb9e6e82608
Andrew <andrew@evo-studio.com>
2024-11-16 20:49:51 +0200

    UPD : Game speed setting interface

--- Assets/Scripts/UI/Gun/SettingGunModel.cs
+++ Assets/Scripts/UI/Gun/SettingGunModel.cs
@@ -1,22 +1,28 @@
 using System;
+using UnityEngine;
 
 public class SettingGunModel
 {
     public event Action<float> onChangedPower;
+    public event Action<float> onChangedSpeed;
 
-    public float Min { get; }
-    public float Max { get; }
+    public float MinPower { get; }
+    public float MaxPower { get; }
     public float Power => _gun.Power;
 
-    private Gun _gun;
+    public float MinSpeed => 0.01f;
+    public float MaxSpeed => 3f;
+    public float Speed { get; private set; } = 1;
+
+    private readonly Gun _gun;
 
 
-    public SettingGunModel(Gun gun, float min, float max)
+    public SettingGunModel(Gun gun, float minPower, float maxPower)
     {
         _gun = gun;
         _gun.onChangedPower += OnChangedPower;
-        Min = min;
-        Max = max;
+        MinPower = minPower;
+        MaxPower = maxPower;
     }
 
     private void OnChangedPower(float power)
@@ -27,5 +33,15 @@ public class SettingGunModel
     public void SetPower(float power)
     {
         _gun.SetPower(power);
+    }
+
+    public void SetSpeed(float speed)
+    {
+        Speed = speed;
+
+        Debug.Log(speed);
+        Time.timeScale = speed;
+
+        onChangedSpeed?.Invoke(speed);
     }
 }
--- Assets/Scripts/UI/Gun/SettingGunUIView.cs
+++ Assets/Scripts/UI/Gun/SettingGunUIView.cs
@@ -8,12 +8,19 @@ public class SettingGunUIView : MonoBehaviour
     [SerializeField] private Slider _sliderPower;
     [SerializeField] private TextMeshProUGUI _txtPower;
 
+    [SerializeField] private Slider _sliderSpeed;
+    [SerializeField] private TextMeshProUGUI _txtSpeed;
+
     private SettingGunModel _model;
+
 
     private void Awake()
     {
         _sliderPower.onValueChanged.AddListener(OnChangedSliderValue);
         _sliderPower.minValue = 0.01f;
+
+        _sliderSpeed.onValueChanged.AddListener(OnChangedSpeedValue);
+        _sliderSpeed.minValue = 0.01f;
     }
 
     public void SetModel(SettingGunModel model)
@@ -21,27 +28,53 @@ public class SettingGunUIView : MonoBehaviour
         if(_model != null)
         {
             _model.onChangedPower -= OnChangedPower;
+            _model.onChangedSpeed -= OnChangedSpeed;
         }
 
         _model = model;
         model.onChangedPower += OnChangedPower;
+        model.onChangedSpeed += OnChangedSpeed;
     }
 
     private void OnChangedSliderValue(float value)
     {
-        float power = ConverterValue.Map(value, 0.01f, 1, _model.Min, _model.Max);
+        float power = ConverterValue.Map(value, 0.01f, 1, _model.MinPower, _model.MaxPower);
         _model.SetPower(power);
+    }
+
+    private void OnChangedSpeedValue(float value)
+    {
+        float speed = ConverterValue.Map(value, 0.01f, 1, _model.MinSpeed, _model.MaxSpeed);
+        _model.SetSpeed(speed);
     }
 
     private void OnChangedPower(float power)
     {
-        UpdateView();
+        UpdateViewPower();
+    }
+
+    private void OnChangedSpeed(float speed)
+    {
+        UpdateViewSpeed();
     }
 
     public void UpdateView()
     {
-        double percent = Math.Round(ConverterValue.Map(_model.Power, _model.Min, _model.Max, 1f, 100f), 0);
+        UpdateViewPower();
+        UpdateViewSpeed();
+    }
+
+    private void UpdateViewPower()
+    {
+        double percent = Math.Round(ConverterValue.Map(_model.Power, _model.MinPower, _model.MaxPower, 1f, 100f), 0);
         _sliderPower.value = (float)percent / 100f;
         _txtPower.text = percent.ToString();
+    }
+
+    private void UpdateViewSpeed()
+    {
+        double percent = Math.Round(ConverterValue.Map(_model.Speed, _model.MinSpeed, _model.MaxSpeed, 1f, 100f), 0);
+        _sliderSpeed.value = (float)percent / 100f;
+        _txtSpeed.text = percent.ToString();
     }
 }
